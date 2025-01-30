53a6ebce72e1b00caceef5e87c6015e4e4bf51e3
Camoralesh27 <camoralesh27@gmail.com>
2025-01-29 22:16:15 -0600

    agregando links a cada producto y titulos de cada sección

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,22 @@
 /*======= SHOW MENU =======*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-          nav = document.getElementById(navId);
+        nav = document.getElementById(navId);
 
     toggle.addEventListener('click', () => {
         // Add show-menu class to nav menu
         nav.classList.toggle('show-menu');
         // Add show-icon to show and hide menu icon
         toggle.classList.toggle('show-icon');
+    });
+
+    // Close menu when clicking on any nav link
+    const navLinks = document.querySelectorAll(`#${navId} .navM__link`);
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('show-menu');
+            toggle.classList.remove('show-icon');
+        });
     });
 };
 
